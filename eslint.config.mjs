e1b399e26a26bbcec31f1d5d4bd68392d70bc6c8
Avi2014 <avinashr.ug23.cs@nitp.ac.in>
2025-01-25 03:54:52 +0530

    api added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+import { version } from "react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: { react: { version: "detect" } },
+  },
+  reactPlugin.configs.flat("jsx-runtime"),
   {
     files: ["**/*.js"],
     languageOptions: {
@@ -14,6 +21,10 @@ export default [
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
