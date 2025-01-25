0ac0d47877b488150d5bd3186bec0182a6ff44c7
Avi2014 <avinashr.ug23.cs@nitp.ac.in>
2025-01-25 17:08:43 +0530

    using hooks: useState

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -8,7 +8,7 @@
 const Pizza = (props) => {
   return (
     <div className="pizza">
-      <h1>{props.name}</h1>
+      <h1>{props.name} </h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
     </div>
